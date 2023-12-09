d265496aa7a399566ad90aa2d52823cb8b878d6d
Vipin9598 <chauhanvicky1906@gmail.com>
2023-12-09 19:43:23 +0530

    abki baar MODI SARKAR

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(express.json());
 app.use(cors({
-    origin:"https://my-portfolio-git-main-vipin9598.vercel.app/",
+    origin:"https://my-portfolio-vipin9598.vercel.app/",
     methods: "GET,POST,PUT,DELETE", 
     credentials:true
 }))
