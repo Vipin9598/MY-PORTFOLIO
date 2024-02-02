56fd8ddac3ac0598050b7ec8f15fccd6d4498da1
Vipin9598 <chauhanvicky1906@gmail.com>
2024-02-02 09:39:31 +0530

    New resume

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,7 @@
 import TypingComponent from "./typing";
 import Button from "./button"
 import image from "../images/cv1.jpeg"
+import Resume from "../images/New_Resume.pdf"
 import {motion,animate} from "framer-motion"
 function main(){
 
@@ -8,13 +9,13 @@ function main(){
     
     return (
         <div className="  mt-[100px] flex md:flex-row flex-col-reverse justify-between items-center gap-5 " id="main">
-            <motion.div initial={{opacity:0,x:-150}} whileInView={{opacity:1,x:0,transition:{duration:1,ease:"easeInOut"}}} className=" text-gray-400 sm:w-7/12 w-11/12 max-w-[480px] mt-8 flex flex-col gap-5">
+            <motion.div  initial={{opacity:0,x:-150}} whileInView={{opacity:1,x:0,transition:{duration:1,ease:"easeInOut"}}} className=" text-gray-400 sm:w-7/12 w-11/12 max-w-[480px] mt-8 flex flex-col gap-5">
                 {/* <p className="text-3xl font-bold text-red-500">MERN DEVLOPER
                 </p> */}
                 <div className="flex flex-col gap-3 text-gray-400 text-3xl my-5">Hi,<br/> I'm Vipin,<br/> <TypingComponent /></div>
                 <div className="text-xl  leading-8 my-8  tracking-wide ">Hey there, I'm Vipin , I'm a final year B.Tech student and a full stack developer with a penchant for turning ideas into interactive digital experiences. Join me as I showcase my journey through a collection of projects that blend creativity and technology.</div>
                 {
-                   <a href="https://drive.google.com/uc?id=175RP24fAfzjkdyJDkZEUZybAAxnzum75" download >
+                   <a href={Resume} download="MY CV" >
                    <Button data={data}></Button>
                </a>
                
